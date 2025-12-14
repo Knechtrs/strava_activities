09b0d7970f0a0c5fd6b2cbcdfda784e9f08cb047
Knechtrs <knecht.raphael@gmail.com>
2025-12-14 20:39:06 +0100

    added theme.R to local folder

---
+++ R/theme_layout.R
@@ -0,0 +1,18 @@
+theme_layout <- theme(
+    plot.background = element_rect(fill="white", color="white"),
+    panel.background = element_rect(fill="white", color="white"),
+    legend.background = element_rect(fill="white", color="white"),
+    strip.background = element_rect(fill="white", color="white"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line.x = element_line(linewidth = 0.5, color="black"),
+    axis.line.y = element_line(linewidth = 0.5, color="black"),
+    axis.ticks = element_line(color = "black"),
+    text = element_text(color = "black"),
+    axis.title = element_text(color = "black"),
+    axis.text = element_text(color = "black"),
+    plot.title = element_text(color = "black"),
+    legend.text = element_text(color = "black"),
+    legend.title = element_text(color = "black"),
+    strip.text = element_text(color = "black")
+)
